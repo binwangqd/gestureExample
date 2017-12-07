f440c8a0af65195fb3f137e1c8d70e610faaf028
Bin Wang <bin.wang@quadinfotech.com>
2017-12-07 09:38:09 -0500

    Initial Commit

---
+++ GestureExample/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  GestureExample
+//
+//  Created by Bin Wang on 2017-12-07.
+//  Copyright © 2017 Bin Wang. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
